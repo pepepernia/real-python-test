b1c8d7263f6dbd7503b9025e61dd1551aea6515d
Jose <josepernia@gmail.com>
2018-01-24 11:20:37 +0100

    after SQL lessons

---
+++ 09_sql.py
@@ -0,0 +1,20 @@
+
+import sqlite3
+
+with sqlite3.connect("new.db") as connection:
+	c = connection.cursor()
+
+	c.execute("""SELECT DISTINCT population.city, population.population, regions.region FROM population, regions WHERE population.city = regions.city ORDER by population.city ASC""")
+
+	rows = c.fetchall()
+
+	for r in rows:
+
+		print("City: " + r[0])
+		print("Population: " + r[1])
+		print("region: " + r[2])
+		print("")
+
+
+
+		
